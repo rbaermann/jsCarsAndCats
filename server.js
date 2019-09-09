8df8b309ca8aef3313276e7af1152ede01a01b1b
Ryan Baermann <rcbaermann@gmail.com>
2019-09-08 18:25:01 -0700

    added cat details

--- server.js
+++ server.js
@@ -22,6 +22,36 @@ app.get('/cats', (req, res) => {
     res.render('cats');
 });
 
+app.get('/tymber', (req, res) => {
+    let dict = {
+        name : "Tymber",
+        favFood : "Salmon",
+        age : 4,
+        sleep : ["On your legs", "Top of tower", "On clothes"]
+    };
+    res.render('details', {cat : dict});
+});
+
+app.get('/aspen', (req, res) => {
+    let dict = {
+        name : "Aspen",
+        favFood : "Chicken",
+        age : 1,
+        sleep : ["Lower part of tower", "Under the bed", "On your chest"]
+    };
+    res.render('details', {cat : dict});
+});
+
+app.get('/ankou', (req, res) => {
+    let dict = {
+        name : "Ankou",
+        favFood : "Bonito Flakes",
+        age : 2,
+        sleep : ["On your lap", "On your face", "Very top of tower"]
+    };
+    res.render('details', {cat : dict});
+});
+
 
 
 app.listen(8000, () => console.log("listening on port 8000"));
