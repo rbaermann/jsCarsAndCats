bcba33709f0e0c136f8a7be4b88c8070d1661d44
Ryan Baermann <rcbaermann@gmail.com>
2019-09-08 17:44:19 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/static"));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+app.get('/cars', (req, res) => {
+    res.render('cars');
+});
+
+app.get('/cars/new', (req, res) => {
+    res.render('form');
+});
+
+app.get('/cats', (req, res) => {
+    res.render('cats');
+});
+
+
+
+app.listen(8000, () => console.log("listening on port 8000"));
